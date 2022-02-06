6dcfc8767e4658035dbda2fd5a011e8049b016fb
yutiborn <yutib@webschool.co.il>
2022-02-06 22:41:05 +0200

    new project random numbers with state

---
+++ src/components/Table.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useState} from 'react';
+
+function isPrime(number){
+  if (number===0||number===1){return false}
+  for(var i = 2;i < number; i++)
+    if (number % i === 0) {return false}
+    return true
+}
+
+
+
+
+
+const Table =(props)=> {
+  let [newNumber,setNewNumber] = useState(props.number)
+  const add =()=>{
+  setNewNumber(newNumber += Math.floor(Math.random()*11))
+  }
+ 
+  return (
+    <div className={isPrime(newNumber)?"scuare yellow":newNumber%2===0?"scuare red":"scuare green"}  onClick={add} >
+        {newNumber}
+         
+    </div>
+  )
+}
+
+
+export {Table}
